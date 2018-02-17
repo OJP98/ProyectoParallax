0e19886cb0f19de57428c799a31b7acc0e963896
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-02-16 21:14:03 -0600

    Implementacion Java UML

---
+++ ParallaxAlgoritmo/src/parallaxalgoritmo/ParallaxAlgoritmo.java
@@ -0,0 +1,34 @@
+/*
+ * ParallaxAlgoritmo.java
+ * Robot camninando por un laberinto.
+ * Andrea Arguello 17801
+ * Oscar Juarez 17315
+ * Mario Sarmientos 17055
+ * 16/02/2018
+ */
+package parallaxalgoritmo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author cooli
+ */
+public class ParallaxAlgoritmo {
+    
+    private static Robot parallax;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        
+        }
+    }
+
+    
+
